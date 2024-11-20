f7eb52a5e8d83abfbdc28e182a9314c01c5d30fb
Jon Rood <jon.rood@nrel.gov>
2024-11-20 16:13:05 -0700

    Updates.

--- submit_jobs.py
+++ submit_jobs.py
@@ -281,10 +281,10 @@ cmd() {
 
     if machine == 'frontier' or machine == 'kestrel-cpu' or machine == 'kestrel-gpu':
         job.script += ("cmd \"module load cray-python\"\n")
-        job.script += ("cmd \"#export FI_MR_CACHE_MONITOR=memhooks\"\n")
-        job.script += ("cmd \"#export FI_CXI_RX_MATCH_MODE=software\"\n")
-        job.script += ("cmd \"#export MPICH_SMP_SINGLE_COPY_MODE=NONE\"\n")
-        job.script += ("cmd \"#export MPICH_GPU_SUPPORT_ENABLED=1\"\n")
+        job.script += ("#cmd \"export FI_MR_CACHE_MONITOR=memhooks\"\n")
+        job.script += ("#cmd \"export FI_CXI_RX_MATCH_MODE=software\"\n")
+        job.script += ("#cmd \"export MPICH_SMP_SINGLE_COPY_MODE=NONE\"\n")
+        job.script += ("#cmd \"export MPICH_GPU_SUPPORT_ENABLED=1\"\n")
         job.script += ("cmd \"" + "export EXAWIND_MANAGER=" + job_set.exawind_manager + "\"\n")
         job.script += ("cmd \"source ${EXAWIND_MANAGER}/start.sh && spack-start\"\n")
         job.script += ("cmd \"spack env activate -d ${EXAWIND_MANAGER}/environments/" + job_set.environment + "\"\n")
